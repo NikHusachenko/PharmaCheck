e0eb9b7485b37820ea86ac9efa21a248fbfbc869
PavloSL12 <156653947+PavloSL12@users.noreply.github.com>
2024-03-13 20:29:49 +0200

    Update
    Tables was set, added foundation for the configuration for the connection and tables.

---
+++ PharmaCheck.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmaCheck.Database.Entities;
+
+namespace PharmaCheck.EntityFramework
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Set tables
+        public DbSet<EntityBase> Entities { get; set; }
+        public DbSet<MedicineEntity> MedicineEntities { get; set;}
+        public DbSet<OrderEntity> OrderEntities { get; set; }
+        public DbSet<PharmacyEntity> PharmacyEntities { get;set; }
+        public DbSet<UserEntity> UserEntities { get; set; }
+
+        //Tables Defined
+        //Ctor/Migrations
+        public ApplicationDbContext()
+        {
+            Database.Migrate();
+        }
+
+        //Configure db connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql();
+        }
+
+        //Configure tables
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EntityBase>();
+            modelBuilder.Entity<MedicineEntity>();
+            modelBuilder.Entity<OrderEntity>();
+            modelBuilder.Entity<PharmacyEntity>();
+            modelBuilder.Entity<UserEntity>();
+        }
+
+    }
+}
